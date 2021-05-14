cefe5843a4c0504bef4cda83561e1da19097b22f
Sunny Lau <sunnylau1206@gmail.com>
2021-05-14 15:35:03 -0700

    add new model for food log

---
+++ models/log-item.js
@@ -0,0 +1,10 @@
+class LogItem {
+  constructor(quantity, mealBrand, mealCalorie, sum) {
+    this.quantity = quantity;
+    this.mealBrand = mealBrand;
+    this.mealCalorie = mealCalorie;
+    this.sum = sum;
+  }
+}
+
+export default LogItem;
