6ecbab52b1c2100135e24dffc1353913a0734032
Sunny Lau <sunnylau1206@gmail.com>
2021-05-18 15:45:30 -0700

    clear conflict

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-<<<<<<< HEAD
-=======
-    plugins: ['react-native-reanimated/plugin'],
->>>>>>> 049f7a204982c277c490ef07db75461626918b96
+    presets: ["babel-preset-expo"],
   };
 };
